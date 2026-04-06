ebf99545fb42a4aac1c8a831e8d19ca353e36a71
SANIYA GUPTA <160046544+S-aniya@users.noreply.github.com>
2026-04-06 17:05:16 +0000

    completed

--- 1.Modules-comments-pip.py
+++ 1.Modules-comments-pip.py
@@ -1,3 +1,30 @@
 print("hello world")
 # module is a file containing code written by somebody else
-# pip is used to install these modules
+# pip is a packet manager used to install these modules
+import pyjokes
+joke=pyjokes.get_joke()
+print(joke)
+print(5+6)
+# practice set questions 
+# q1. Write a program to print twinkle twinkle little star poem in python
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Then the traveller in the dark
+Thanks you for your tiny spark;
+He could not see which way to go,
+If you did not twinkle so.
+Twinkle, twinkle, little star,
+How I wonder what you are!''')
+
+print("*****************************************************************")
+
+#use REPL and print the table of 5 using it
+# done on terminal
+# exit() ---> get out of REPL 
+# pyttsx3 ---> module used for text to speech
