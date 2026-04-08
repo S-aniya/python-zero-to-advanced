f2133a2edce35a0a2749370e026d33f5de0a8ef5
SANIYA GUPTA <160046544+S-aniya@users.noreply.github.com>
2026-04-08 06:29:01 +0000

    Truth table

---
+++ 4.Variables_and_datatypes.py
@@ -0,0 +1,14 @@
+a=2
+b=8
+c="hi"
+d=78.34
+print(a+b+d) #we can add integer and float
+#types of datatypes:
+# integers
+# strings
+# floating points
+# boolean
+# none
+e=True
+f=None
+
---
+++ 5.Truth_table.py
@@ -0,0 +1,11 @@
+#AND values truth table 
+print("True and True : ",True and True)
+print("True and False : ",True and False)
+print("False and True : ",False and True)
+print("False and False : ",False and False)
+print()
+#OR values truth table 
+print("True or True : ",True or True)
+print("True or False : ",True or False)
+print("False or True : ",False or True)
+print("False or False : ",False or False)
