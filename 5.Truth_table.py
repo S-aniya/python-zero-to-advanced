410e0021cac89fa07b695caef0e36f02f46688e8
SANIYA GUPTA <160046544+S-aniya@users.noreply.github.com>
2026-04-08 06:44:01 +0000

    implemented the truth table in normal and tabular format

--- 5.Truth_table.py
+++ 5.Truth_table.py
@@ -9,3 +9,14 @@ print("True or True : ",True or True)
 print("True or False : ",True or False)
 print("False or True : ",False or True)
 print("False or False : ",False or False)
+
+#trying the tabular format
+
+from tabulate import tabulate
+data=[
+    ["True","True",True and True],
+    ["True","False",True and False],
+    ["False","True",False and True],
+    ["False","False",False and False],
+]
+print(tabulate(data,headers=["    "," AND","    "],tablefmt="grid"))
